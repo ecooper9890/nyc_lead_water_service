2bd5ba73c37a8cde0871c9da24f1d7d4b19358a5
eric-r-cooper <37560744+eric-r-cooper@users.noreply.github.com>
2020-02-04 10:55:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app.layout = html.Div(children=[
     columns=[{"name": i, "id": i} for i in df.columns],
     data=df.to_dict('records'),style={'width': '49%', 'display': 'inline-block'}
 ),
-    html.Div(id='prediction'),
+    html.Div(id='prediction')
 ])
 
 
