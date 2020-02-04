fa4686c094ea0323d3bb297125d0127dc1af2892
eric-r-cooper <37560744+eric-r-cooper@users.noreply.github.com>
2020-02-04 12:30:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app.title='NYC Lead Water Service Lines'
 app.layout = dash_table.DataTable(
     id='table',
     columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.head().to_dict('records'),
+    data=df.head().to_dict('records')
 )
 #app.layout = html.Div(children=[
 #    html.H1(children='New York City Lead Service Line Locator'),
