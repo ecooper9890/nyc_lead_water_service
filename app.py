93e3bb35424fbbb97560ff5d032497a818fff03f
eric-r-cooper <37560744+eric-r-cooper@users.noreply.github.com>
2020-02-04 21:18:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 
 import dash
-import dash-table as dt
+import dash_table as dt
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -12,7 +12,9 @@ import geopandas as gpd
 import folium
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+
 url = 'https://raw.githubusercontent.com/eric-r-cooper/my_insight_app/master/APP_DATA.csv'
+
 
 #df = pd.read_csv(url, error_bad_lines=False)
 t1 = pd.read_csv('https://raw.githubusercontent.com/eric-r-cooper/nyc_lead_water_service/master/APP_DATA_address.csv',error_bad_lines=False)
@@ -24,7 +26,12 @@ df = df.merge(t3)
 df = df.merge(t4)
 df.drop(columns=['Unnamed: 0'],inplace=True)
 
+df['Prediction'].replace(1,'Lead Service Line',inplace=True)
+df['Prediction'].replace(0,'Non-Lead Service Line',inplace=True)
+df.rename(columns={'latitude':'Latitude','longitude':'Longitude','YearBuilt':'Construction Year','zip':'Zip Code'},inplace=True)
+
 ny_map = folium.Map(location=[40.682, -73.945],tiles='Stamen Toner' ,zoom_start=10)
+
 
 boros = ["Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -32,18 +39,33 @@ server = app.server
 app.title='NYC Lead Water Service Lines'
 
 
+#app.layout = dt.DataTable(
+#    id='table',
+#    columns=[{"name": i, "id": i} for i in df.columns],
+#    data=df.to_dict('records'),
+#)
+
+
 app.layout = html.Div(children=[
     html.H1(children='New York City Lead Service Line Locator'),
 
-    html.Label(["Select Service Line Material:", dcc.RadioItems(id="service",options=[{'label': 'Lead Service Line', 'value': 'Lead'},{'label': 'Non-Lead Service Line', 'value': 'No Lead'}], labelStyle={'display': 'inline-block'})]),	
+
+    html.Label(["Select Service Line Material:", dcc.RadioItems(id="service",options=[{'label': 'Lead Service Line', 'value': 'Lead'},{'label': 'Non-Lead Service Line', 'value': 'No Lead'}], labelStyle={'display': 'inline-block'})]),        
     html.Label(["Select Borough:", dcc.RadioItems(id="boro",options=[{'label': i, 'value': i} for i in boros],labelStyle={'display': 'inline-block'})]),
     html.Label(["Select Zip Code:",dcc.Dropdown(id="zip")]),
-    html.Label(["Select Your Street Address:",dcc.Dropdown(id="staddr")]),
+    #html.Label(["Select Your Street Address:",dcc.Dropdown(id="staddr")]),
     html.Iframe(id='map', srcDoc=ny_map._repr_html_(), width='50%',height='400',style={'width': '49%', 'display': 'inline-block'}),
-    html.Div(id='prediction',style={'width': '49%', 'display': 'inline-block'}),
-    dt.DataTable(id='table',columns=[{"name": i, "id": i} for i in df.columns],data=df.head().to_dict('records'),style={'width': '49%', 'display': 'inline-block'})
-    #html.Div([dash_table.DataTable(id='tweet_table', rows=[{}])])    
+    dt.DataTable(id='table',columns=[{"name": i, "id": i} for i in df.columns],data=df.head().to_dict('records'))
+    #html.Div(id='prediction')
 ])
+
+
+@app.callback(
+    dash.dependencies.Output("table", "data"),
+    [dash.dependencies.Input("zip","value")],
+)
+def update_options(value):
+    return df[df['Zip Code']==value].head().to_dict('records')
 
 
 #@app.callback(
@@ -51,9 +73,11 @@ app.layout = html.Div(children=[
 #    [dash.dependencies.Input("service","value")],
 #)
 #def update_options(value):
-#    if (value == 'Non Lead'): df_service = df[df['Prediction']==0]
-#    if (value == 'Lead'): df_service = df[df['Prediction']==1]
+#    if (value == 'Non Lead'): df_service = df[df['Prediction']=='Non-Lead Service Line']
+#    if (value == 'Lead'): df_service = df[df['Prediction']=='Lead Service Line']
 #    return [{'label': i, 'value': i} for i in boros]
+
+
 
 
 @app.callback(
@@ -61,48 +85,60 @@ app.layout = html.Div(children=[
     [dash.dependencies.Input("boro","value"), dash.dependencies.Input("service","value")],
 )
 def update_options(value1,value2):
-    if (value2 == 'No Lead'): df_service = df[df['Prediction']==0]
-    else: df_service = df[df['Prediction']==1]
-    return [{'label': i , 'value': i} for i in df_service[df_service['Borough']==value1]['zip'].unique()]
+    if (value2 == 'No Lead'): df_service = df[df['Prediction']=='Non-Lead Service Line']
+    else: df_service = df[df['Prediction']=='Lead Service Line']
+    return [{'label': i , 'value': i} for i in df_service[df_service['Borough']==value1]['Zip Code'].unique()]
 
-@app.callback(
-    dash.dependencies.Output("staddr", "options"),
-    [dash.dependencies.Input("zip","value"),dash.dependencies.Input(component_id = 'service',component_property = "value")],
-)
-def update_options(value1, value2):
-    if (value2 == 'No Lead'): df_service = df[df['Prediction']==0]
-    else: df_service = df[df['Prediction']==1]
-    return [{'label': i , 'value': i} for i in df_service[df_service['zip']==value1]['Address'].unique()]
 
-@app.callback(
-    dash.dependencies.Output(component_id='prediction', component_property='children'),
-    [dash.dependencies.Input(component_id='staddr', component_property='value'),dash.dependencies.Input(component_id='service', component_property='value')]
-)
-def update_output_div(input_value1,input_value2):
-    if isinstance(input_value1, type(None)): return ['']
-    if (input_value2 == 'No Lead'): a=0
-    else: a=1
-    if (input_value2 == 'No Lead'): df_service = df[df['Prediction']==0]
-    else: df_service = df[df['Prediction']==1]
-    if (input_value1 not in list(df[df['Prediction']==a].Address)): return[' ']
-    return ['This address is predicted to have service line of type: ' + input_value2]	
-    #p = list(df_service[df_service['Address']==input_value1]['Prediction'])[0]
+#@app.callback(
+#    dash.dependencies.Output("staddr", "options"),
+#    [dash.dependencies.Input("zip","value"),dash.dependencies.Input(component_id = 'service',component_property = "value")],
+#)
+#def update_options(value1, value2):
+#    if (value2 == 'No Lead'): df_service = df[df['Prediction']=='Non-Lead Service Line']
+#    else: df_service = df[df['Prediction']=='Lead Service Line']
+#    return [{'label': i , 'value': i} for i in df_service[df_service['Zip Code']==value1]['Address'].unique()]
+
+
+#@app.callback(
+#    dash.dependencies.Output(component_id='prediction', component_property='children'),
+#    [dash.dependencies.Input(component_id='staddr', component_property='value'),dash.dependencies.Input(component_id='service', component_property='value')]
+#)
+#def update_output_div(input_value1,input_value2):
+#    if isinstance(input_value1, type(None)): return ['']
+#    if (input_value2 == 'No Lead'): a=0
+#    else: a=1
+#    if (input_value2 == 'No Lead'): df_service = df[df['Prediction']=='Non-Lead Service Line']
+#    else: df_service = df[df['Prediction']=='Lead Service Line']
+#    if (input_value1 not in list(df[df['Prediction']==a].Address)): return[' ']
+#    return ['This address is predicted to have service line of type: ' + input_value2]        
+#    p = list(df_service[df_service['Address']==input_value1]['Prediction'])[0]
     #if p==0: return ['This address is predicted to have: NOT LEAD']
     #if p==1: return ['This address is predicted to have: LEAD']    
 
+
 @app.callback(
     dash.dependencies.Output(component_id='map', component_property='srcDoc'),
-    [dash.dependencies.Input(component_id='staddr', component_property='value'),dash.dependencies.Input(component_id='service', component_property='value')]
+    [State('table','active_cell')]
 )
 def update_output_div(input_value1,input_value2):
-    if isinstance(input_value1, type(None)): return [folium.Map(location=[40.682, -73.945],tiles='Stamen Toner' ,zoom_start=10)._repr_html_()]
+    active_cell_row_index = ''
+    if active_cell:
+        active_cell_row_index = active_cell[0]
+    r = df.iloc[active_cell_row_index]	
+
+    a = folium.Map(location=[40.682, -73.945],tiles='Stamen Toner' ,zoom_start=10)
+    a = ny_map._repr_html_()	
+    if isinstance(input_value1, type(None)): return(ny_map._repr_html_())
     new_map = folium.Map(location=[40.682, -73.945],tiles='Stamen Toner' ,zoom_start=10)
-    lat = df[df['Address']==input_value1].iloc[0]['latitude']
-    lon = df[df['Address']==input_value1].iloc[0]['longitude']
-    if (df[df['Address']==input_value1].iloc[0]['Prediction'] == 1): marker = folium.Marker(location=[lat, lon],icon=folium.Icon(color='red'))
+    lat = r['Latitude']
+    lon = r['Longitude']
+    if (df[df['Address']==input_value1].iloc[0]['Prediction'] == 'Lead'): marker = folium.Marker(location=[lat, lon],icon=folium.Icon(color='red'))
     else: marker = folium.Marker(location=[lat, lon],icon=folium.Icon(color='blue'))
     marker.add_to(new_map)
-    return [new_map._repr_html_()]
+    b = new_map._repr_html_()
+    return(new_map._repr_html_())
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
